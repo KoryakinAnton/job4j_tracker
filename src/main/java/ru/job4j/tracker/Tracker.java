29821287ac4485f4408ee7cf49a92a12ba93b7e4
Anton <anton.koryakin@mail.ru>
2021-02-17 15:13:05 +0300

    5. Tracker - хранилище [#460303]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,31 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[size];
+        int sizeWithoutNull = 0;
+        for (int index = 0; index < size; index++) {
+            if (items[index] != null) {
+                itemsWithoutNull[sizeWithoutNull] = items[index];
+                sizeWithoutNull++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutNull, sizeWithoutNull);
+    }
+
+    public Item[] findByName(String key){
+        Item[] namesKey = new Item[size];
+        int sizeOfNames = 0;
+        for (int index = 0; index < size; index++) {
+            String tmp = items[index].getName();
+            if (tmp.equals(key)) {
+                namesKey[sizeOfNames] = items[index];
+                sizeOfNames++;
+            }
+        }
+        return Arrays.copyOf(namesKey, sizeOfNames);
     }
 
     public Item findById(int id) {
