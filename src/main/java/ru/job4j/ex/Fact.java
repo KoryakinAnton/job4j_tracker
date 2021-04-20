ed62cdadaadb8186328b12627b2cfbd28d39e95a
Anton <anton.koryakin@mail.ru>
2021-04-20 12:59:50 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#460243]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class Fact {
+
+    public static int calc(int n) {
+        int rsl = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial should be positive.");
+        } else {
+            for (int index = 1; index <= n; index++) {
+                rsl *= index;
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial of 3 is : " + calc( 3));
+    }
+}
